794198fd22e3da9f8d60bf6340c00c60168ae923
Pyked <pybasket07@gmail.com>
2023-12-22 19:24:18 +0000

    Exercice SSl Terminé

--- code/tp3-heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
+++ code/tp3-heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
@@ -2,9 +2,92 @@ package fr.istic.vv;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryHeapTest {
 
+    @Test
+    void testBinaryHeapConstructor() {
+        // Block: Valid comparator
+        BinaryHeap<Integer> heap1 = new BinaryHeap<>(Comparator.naturalOrder());
+        assertNotNull(heap1);
 
+        // Block: Null comparator
+        assertThrows(IllegalArgumentException.class, () -> new BinaryHeap<>(null));
+    }
+
+    @Test
+    void testPop() {
+        BinaryHeap<Integer> heap = new BinaryHeap<>(Comparator.naturalOrder());
+
+        // Block: Empty heap
+        assertThrows(NoSuchElementException.class, heap::pop);
+
+        // Block: Heap with one element
+        heap.push(5);
+        assertEquals(5, heap.pop());
+
+        // Block: Heap with multiple elements
+        heap.push(10);
+        heap.push(5);
+        heap.push(15);
+        assertEquals(5, heap.pop());
+    }
+
+    @Test
+    void testPeek() {
+        BinaryHeap<Integer> heap = new BinaryHeap<>(Comparator.naturalOrder());
+
+        // Block: Empty heap
+        assertThrows(NoSuchElementException.class, heap::peek);
+
+        // Block: Heap with one element
+        heap.push(5);
+        assertEquals(5, heap.peek());
+
+        // Block: Heap with multiple elements
+        heap.push(10);
+        heap.push(5);
+        heap.push(15);
+        assertEquals(5, heap.peek());
+    }
+
+    @Test
+    void testPush() {
+        BinaryHeap<Integer> heap = new BinaryHeap<>(Comparator.naturalOrder());
+
+        // Block: Null element
+        assertThrows(NullPointerException.class, () -> heap.push(null));
+
+        // Block: First element in the heap
+        heap.push(5);
+        assertEquals(5, heap.peek());
+
+        // Block: Not the first element in the heap
+        heap.push(3);
+        assertEquals(3, heap.peek());
+        heap.push(7);
+        assertEquals(3, heap.peek());
+    }
+
+    @Test
+    void testCount() {
+        BinaryHeap<Integer> heap = new BinaryHeap<>(Comparator.naturalOrder());
+
+        // Block: Empty heap
+        assertEquals(0, heap.count());
+
+        // Block: Heap with one element
+        heap.push(5);
+        assertEquals(1, heap.count());
+
+        // Block: Heap with multiple elements
+        heap.push(10);
+        heap.push(5);
+        heap.push(15);
+        assertEquals(4, heap.count());
+    }
 }
--- code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
+++ code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
@@ -1,15 +1,46 @@
 package fr.istic.vv;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class TLSSocketFactoryTestMocks {
 
+    @Test
+    public void preparedSocket_NullProtocols() {
+        TLSSocketFactory tlsSocketFactory = new TLSSocketFactory();
+        SSLSocket mockSocket = mock(SSLSocket.class);
 
+        tlsSocketFactory.prepareSocket(mockSocket);
+
+        // Verify that no methods were called on the mock socket
+        verifyZeroInteractions(mockSocket);
+    }
+
+    @Test
+    public void typical() {
+        TLSSocketFactory tlsSocketFactory = new TLSSocketFactory();
+        SSLSocket mockSocket = mock(SSLSocket.class);
+
+        // Stubbing the behavior of the mock socket
+        when(mockSocket.getSupportedProtocols()).thenReturn(shuffle(new String[]{"SSLv2Hello", "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"}));
+        when(mockSocket.getEnabledProtocols()).thenReturn(shuffle(new String[]{"SSLv3", "TLSv1"}));
+
+        tlsSocketFactory.prepareSocket(mockSocket);
+
+        // Verifying that setEnabledProtocols was called with the expected arguments
+        verify(mockSocket).setEnabledProtocols(new String[]{"TLSv1.2", "TLSv1.1", "TLSv1", "SSLv3"});
+    }
+
+    private String[] shuffle(String[] in) {
+        List<String> list = new ArrayList<>(Arrays.asList(in));
+        Collections.shuffle(list);
+        return list.toArray(new String[0]);
+    }
 }
